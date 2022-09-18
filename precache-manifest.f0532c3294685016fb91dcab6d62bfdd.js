3f86ef134d0ad205bdb717aa0391f6f9886c4f09
Vishal Kumar <78551354+Vishujoshi@users.noreply.github.com>
2022-09-18 22:54:29 +0530

    Updates

---
+++ precache-manifest.f0532c3294685016fb91dcab6d62bfdd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "58ba9be1ce8a57f62851",
+    "url": "/E-shop/static/js/main.58ba9be1.chunk.js"
+  },
+  {
+    "revision": "eeb3104a4ac87b8b8f9b",
+    "url": "/E-shop/static/js/1.eeb3104a.chunk.js"
+  },
+  {
+    "revision": "baa50c605db8d23ff8d9",
+    "url": "/E-shop/static/js/runtime~main.baa50c60.js"
+  },
+  {
+    "revision": "4385cc973a1968d9ffc66a76a2d311e3",
+    "url": "/E-shop/index.html"
+  }
+];
